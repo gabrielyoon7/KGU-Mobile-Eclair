705b3fcfba6dc320bacfa28cb647019fe8fcb18e
RainRang <jasd0622@hanmail.net>
2021-11-11 19:49:52 +0900

    BlockButton

---
+++ app/src/main/java/kr/ac/kyonggi/eclair/BlockButton.java
@@ -0,0 +1,46 @@
+package kr.ac.kyonggi.eclair;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+public class BlockButton extends Button{
+    int x;
+    int y;
+    int neighborMines=0;
+    boolean mine=false;
+    boolean flag = false;
+    static int flags;
+    static int blocks;
+
+    public BlockButton(Context context, int x, int y) {
+        super(context);
+        this.x=x;
+        this.y=y;
+    }
+
+    public void toggleFlag(){
+        if(flag){
+            flag=false;
+            flags--;
+            setText("");
+        }else{
+            flag=true;
+            flags++;
+            setText("+");
+        }
+    }
+
+    public boolean breakBlock(View view){
+        if(mine){
+            //display mine
+
+            return true;
+        }else{
+            //number of mines around
+            blocks--;
+            return false;
+        }
+
+    }
+}
--- app/src/main/java/kr/ac/kyonggi/eclair/GameActivity.java
+++ app/src/main/java/kr/ac/kyonggi/eclair/GameActivity.java
@@ -2,12 +2,16 @@ package kr.ac.kyonggi.eclair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+
 public class GameActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +21,37 @@ public class GameActivity extends AppCompatActivity {
         TableLayout table;
         table =(TableLayout) findViewById(R.id.tableLayout);
 
-        Button[][] buttons = new Button[9][9];
+        BlockButton[][] buttons = new BlockButton[9][9];
 
         for(int i = 0; i<9; i++){
             TableRow tableRow = new TableRow(this);
             for(int j=0; j<9; j++){
-                buttons[i][j] = new Button(this);
+                buttons[i][j] = new BlockButton(this, i, j);
                 TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
                         TableRow.LayoutParams.WRAP_CONTENT,
                         TableRow.LayoutParams.WRAP_CONTENT,
                         1.0f);
                 buttons[i][j].setLayoutParams(layoutParams);
                 tableRow.addView(buttons[i][j]);
+
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view){
+                        ((BlockButton)view).breakBlock(view);
+                    }
+                });
+
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view){
+                        ((BlockButton)view).toggleFlag();
+                    }
+                });
             }
             table.addView(tableRow);
         }
+
+
     }
 }
+
