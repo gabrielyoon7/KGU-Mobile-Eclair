fbcb3a281f2e1d49d863dd3b9aef9f0616884ca1
RainRang <jasd0622@hanmail.net>
2021-11-21 16:45:31 +0900

    최종

--- app/src/main/java/kr/ac/kyonggi/eclair/GameActivity.java
+++ app/src/main/java/kr/ac/kyonggi/eclair/GameActivity.java
@@ -23,8 +23,6 @@ public class GameActivity extends AppCompatActivity {
     ToggleButton tbtn = null;
     BlockButton[][] buttons = new BlockButton[9][9];
     TextView mineNum = null;
-//    int iForButton = 0;
-//    int jForButton =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,14 +107,6 @@ public class GameActivity extends AppCompatActivity {
          * */
 
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                System.out.printf(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-
         for (int i = 0; i < 9; i++) {
             TableRow tableRow = new TableRow(this);
             for (int j = 0; j < 9; j++) {
@@ -128,8 +118,8 @@ public class GameActivity extends AppCompatActivity {
                 buttons[i][j].setLayoutParams(layoutParams);
                 tableRow.addView(buttons[i][j]);
 
-                buttons[i][j].x=i;
-                buttons[i][j].y=j;
+                buttons[i][j].x = i;
+                buttons[i][j].y = j;
 
                 if (map[i][j] == -1) {
                     buttons[i][j].mine = true;
@@ -147,7 +137,7 @@ public class GameActivity extends AppCompatActivity {
 
                         } else {
 //                            boolean gameOver = ((BlockButton) view).breakBlock(view);
-                            if (((BlockButton)view).mine) {
+                            if (((BlockButton) view).mine) {
                                 //전부 오픈하는 메소드
 
                                 for (int i = 0; i < buttons.length; i++) {
@@ -173,16 +163,18 @@ public class GameActivity extends AppCompatActivity {
                                     }
                                 });
                                 dlg.show();
-                            }else{// recursive
-                                if(buttons[((BlockButton) view).getBX()][((BlockButton) view).getBY()].neighborMines==0){
+                            } else {// recursive
+                                if (buttons[((BlockButton) view).getBX()][((BlockButton) view).getBY()].neighborMines == 0) {
                                     //
-                                    uncoverNeighbors(((BlockButton) view).getBX(), ((BlockButton) view).getBY(),view);
+                                    uncoverNeighbors(((BlockButton) view).getBX(), ((BlockButton) view).getBY(), view);
+                                } else {
+                                    buttons[((BlockButton) view).getBX()][((BlockButton) view).getBY()].breakBlock(view);
                                 }
                             }
 
                         }//else끝
 
-                        if(BlockButton.flags == 0 && BlockButton.blocks==10){
+                        if (BlockButton.flags == 0 && BlockButton.blocks == 10) {
                             AlertDialog.Builder dlg = new AlertDialog.Builder(GameActivity.this);
                             dlg.setTitle("You Win"); //제목
                             dlg.setMessage("지뢰를 모두 발견했습니다."); // 메시지
@@ -196,35 +188,29 @@ public class GameActivity extends AppCompatActivity {
                         }//if문 종료
 
                     }
-                    void uncoverNeighbors(int i, int j, View view){
-                        boolean a= (i<0||i>8||j<0||j>8);
 
-                        System.out.println(i+""+j);
-                        System.out.println(a);
-
-                        if(a) {
+                    void uncoverNeighbors(int i, int j, View view) {
+                        boolean a = (i < 0 || i > 8 || j < 0 || j > 8);
+                        if (a) {
                             return;
-                        }
-                        else{
-                            boolean b = buttons[i][j].isEnabled()==false;
-                            boolean c =buttons[i][j].neighborMines==-1;
-                            if(b||c){
-                                System.out.println(b);
-                                System.out.println(c);
+                        } else {
+                            boolean b = buttons[i][j].isEnabled() == false;
+                            boolean c = buttons[i][j].neighborMines == -1;
+                            if (b || c) {
+                                return;
+                            } else {
+                                buttons[i][j].breakBlock(view);
+                                if (buttons[i][j].neighborMines == 0) {
+                                    uncoverNeighbors(i + 1, j, view); //right
+                                    uncoverNeighbors(i, j + 1, view); //bottom
+                                    uncoverNeighbors(i + 1, j + 1, view);//rightBottom
+                                    uncoverNeighbors(i - 1, j, view); //left
+                                    uncoverNeighbors(i, j - 1, view); //top
+                                    uncoverNeighbors(i - 1, j - 1, view); //leftTop
+                                    uncoverNeighbors(i + 1, j - 1, view); //rightTop
+                                    uncoverNeighbors(i - 1, j + 1, view); //leftBottom
+                                }
                             }
-                            else{
-                            buttons[i][j].breakBlock(view);
-                            if(buttons[i][j].neighborMines==0) {
-                                uncoverNeighbors(i + 1, j, view); //right
-                                uncoverNeighbors(i, j + 1, view); //bottom
-                                uncoverNeighbors(i + 1, j + 1, view);//rightBottom
-                                uncoverNeighbors(i - 1, j, view); //left
-                                uncoverNeighbors(i, j - 1, view); //top
-                                uncoverNeighbors(i - 1, j - 1, view); //leftTop
-                                uncoverNeighbors(i + 1, j - 1, view); //rightTop
-                                uncoverNeighbors(i - 1, j + 1, view); //leftBottom
-                            }
-                        }
                         }
                     }
                     // recursive 함수
@@ -234,86 +220,4 @@ public class GameActivity extends AppCompatActivity {
             table.addView(tableRow);
         }
     }
-
-
-
-
-
-//    public class ButtonOnClickListener implements View.OnClickListener
-//    {
-//
-//        int i;
-//        int j;
-//        public ButtonOnClickListener(int i, int j) {
-//            this.i = i;
-//            this.j = j;
-//        }
-//
-//        @Override
-//        public void onClick(View view)
-//        {
-//            if (tbtn.isChecked()) {
-//                ((BlockButton) view).toggleFlag();
-//                System.out.println(BlockButton.flags);
-//                mineNum.setText(BlockButton.flags + "");
-//
-//            } else {
-//                boolean gameOver = ((BlockButton) view).breakBlock(view);
-//                if (gameOver) {
-//                    //전부 오픈하는 메소드
-//
-//                    for (int i = 0; i < buttons.length; i++) {
-//                        for (int j = 0; j < buttons[i].length; j++) {
-//                            if (buttons[i][j].mine) {
-//                                buttons[i][j].setText("*");
-//                                buttons[i][j].setEnabled(true);
-//                            } else {
-//                                buttons[i][j].setText(buttons[i][j].neighborMines + "");
-//                                buttons[i][j].setEnabled(true);
-//                            }
-//                        }
-//                    }
-//                    //게임 종료
-//
-//                    AlertDialog.Builder dlg = new AlertDialog.Builder(GameActivity.this);
-//                    dlg.setTitle("GameOver"); //제목
-//                    dlg.setMessage("게임이 종료되었습니다."); // 메시지
-//                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            //토스트 메시지
-//                            Toast.makeText(GameActivity.this, "확인을 누르셨습니다.", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//                    dlg.show();
-//                }else{ // recursive
-//
-//                    if(int i=0; i<9; i++){
-//
-//                    }
-//
-//
-//                }
-//
-//
-//
-//
-//            }//else끝
-//
-//            if(BlockButton.flags == 0 && BlockButton.blocks==10){
-//                AlertDialog.Builder dlg = new AlertDialog.Builder(GameActivity.this);
-//                dlg.setTitle("You Win"); //제목
-//                dlg.setMessage("지뢰를 모두 발견했습니다."); // 메시지
-//                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        //토스트 메시지
-//                        Toast.makeText(GameActivity.this, "확인을 누르셨습니다.", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//                dlg.show();
-//            }//if문 종료
-//        }
-//
-//    };
 }
-
-
